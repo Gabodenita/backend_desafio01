fe0d7bd21794e688018f581b188c5886bd70e5ab
Gabriel Denita <gabriel.denita@gmail.com>
2023-06-22 01:43:06 -0300

    Se agregaron validaciones para todos los campos, se agregó mensaje al ser agregado el producto,falta modificar el contador de id

--- desafio01.js
+++ desafio01.js
@@ -3,12 +3,16 @@ class ProductManager{
         this.product=[]
     }
     addProduct({title, description, price, thumbnail, code, stock}){   
-        const id= this.product.length+1
-        const producto = this.product.find(e => e.code === code)
+        
+        if(title=="" || title== undefined || description=="" ||description == undefined || price=="" || price == undefined || thumbnail== "" || thumbnail== undefined || code=="" || code== undefined || stock== 0 || stock == undefined ){
+            return console.log("Debe ingresar todoso los campos del producto")
+        }else{
+            
+            const id= this.product.length+1
+            const producto = this.product.find(e => e.code === code)
             if (producto){
                 return console.log("el codigo es invalido")
-            }
-            if (!producto){
+            }else if (!producto){
                 this.product.push({
                     id,
                     title,
@@ -18,13 +22,20 @@ class ProductManager{
                     code,
                     stock
                 })
+                console.log(this.product[this.product.length-1].id)
             }
+            
+        }
+
     } 
       
     getProduct() { 
         return this.product
     }
 
+    getProduct_length() { 
+        return this.product.length
+    }
     getProductById(productId) {
         const producto = this.product.find(e => e.id === productId)
         if (!producto){
@@ -38,12 +49,12 @@ class ProductManager{
 }
 
 const p =new ProductManager()
-p.addProduct({ title: 'auto', description: "cuatro puertas", price: "100", thumbnail: false, code: "ABC1", stock: 20})
-p.addProduct({ title: 'auto', description: "2 puertas", price: "10", thumbnail: false, code: "ABC2", stock: 10})
-p.addProduct({ title: 'auto', description: "3 puertas", price: "500", thumbnail: false, code: "ABC1", stock: 5})
-p.addProduct({ title: 'moto', description: "0 puertas", price: "500", thumbnail: false, code: "ABC3", stock: 5})
-p.addProduct({ title: 'camioneta', description: "3 puertas", price: "500", thumbnail: false, code: "ABC4", stock: 5})
+ p.addProduct({ title: 'aaaaaa', description: "cuatro puertas", price: "100", thumbnail: "dasfasdadsa", code: "ABC1", stock: 20})
+ p.addProduct({ title: 'asdfasd', description: "aadsfasdasszxcvz", price: "10", thumbnail: "asdfasd", code: "ABC2" , stock: 55})
+ p.addProduct({ title: 'auto', description: "3 puertas", price: "500", thumbnail: "dasfasdadsa", code: "ABC6", stock: 5})
+ p.addProduct({ title: 'moto', description: "0 puertas", price: "500", thumbnail: "dasfasdadsa", code: "ABC3", stock: 5})
+ p.addProduct({ title: 'camioneta', description: "3 puertas", price: "500", thumbnail: "dasfasdadsa", code: "ABC4", stock: 5})
 
 
 console.log(p.getProduct())
-console.log(p.getProductById(3))
+// console.log(p.getProductById(3))
